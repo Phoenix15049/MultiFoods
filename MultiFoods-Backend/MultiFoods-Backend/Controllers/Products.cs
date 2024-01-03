32aacade04ce1eb1e14c4f9620ad86a2265ebc0a
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-01-03 14:59:45 +0330

    minor changes.

---
+++ MultiFoods-Backend/MultiFoods-Backend/Controllers/Products.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiFoods_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Products : ControllerBase
+    {
+        [HttpGet("Proc")]
+        public string Getproducts()
+        {
+            return "No Product";
+        }
+
+
+    }
+}
