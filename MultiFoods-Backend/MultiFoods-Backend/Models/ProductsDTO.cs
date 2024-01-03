8b950e2a64e2b5ffccdc8fe2af03227e608245a2
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-01-03 15:02:54 +0330

    minor changes.

---
+++ MultiFoods-Backend/MultiFoods-Backend/Models/ProductsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiFoods_Backend.Models
+{
+    public class ProductsDTO
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public int product_price { get; set;
+        }
+    }
+}
