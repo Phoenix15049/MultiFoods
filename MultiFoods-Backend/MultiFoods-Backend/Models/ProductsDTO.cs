6e95dd78137ea905b9a130e1e2f67e629704c909
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-01-04 13:30:59 +0330

    db connected.

--- MultiFoods-Backend/MultiFoods-Backend/Models/ProductsDTO.cs
+++ MultiFoods-Backend/MultiFoods-Backend/Models/ProductsDTO.cs
@@ -4,7 +4,6 @@
     {
         public int product_id { get; set; }
         public string product_name { get; set; }
-        public int product_price { get; set;
-        }
+        public int product_price { get; set;}
     }
 }
