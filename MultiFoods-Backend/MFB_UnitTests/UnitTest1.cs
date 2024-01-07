cb8eb46848d74a901a930d89b107a44ae439dd3d
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-01-07 18:05:48 +0330

    UnitTests Added.

---
+++ MultiFoods-Backend/MFB_UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using MultiFoods_Backend.Controllers;
+
+namespace MFB_UnitTests
+{
+    public class UnitTest1
+    {
+        Products _controller;
+
+        [Fact]
+        public void AllTests()
+        {
+            Assert.Equal(5, 5);
+        }
+    }
+}
