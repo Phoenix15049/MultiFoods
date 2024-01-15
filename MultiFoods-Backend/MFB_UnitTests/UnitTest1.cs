aeffa6b34cb86dc6ca2d0f31d59276c036610716
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-01-15 13:03:15 +0330

    minor Updates

--- MultiFoods-Backend/MFB_UnitTests/UnitTest1.cs
+++ MultiFoods-Backend/MFB_UnitTests/UnitTest1.cs
@@ -2,16 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using MultiFoods_Backend.Controllers;
 using MultiFoods_Backend.Models;
 
+
 namespace MFB_UnitTests
 {
     public class UnitTest1
     {
+        ProductsDTO sample = new ProductsDTO() { product_id = 6, product_name = "string", product_price = 0 };
+        
         Products _controller;
         [Fact]
         public async Task GetProducts_ReturnsStatusCode200()
         {
             // Arrange
-            var controller = new Products(); // Assuming no constructor parameters needed
+            var controller = new Products(); 
 
             // Act
             var result = await controller.GetProducts();
@@ -25,24 +28,24 @@ namespace MFB_UnitTests
         public async Task UpdateProduct_ReturnsStatusCode204()
         {
             // Arrange
-            var controller = new Products(); // Assuming no constructor parameters needed
+            var controller = new Products();
 
             // Act
-            var result = await controller.UpdateProduct(1, new ProductsDTO() { product_id = 5, product_name = "x", product_price = 4 }); // Pass an appropriate ID and DTO
+            var result = await controller.UpdateProduct(10, sample); 
 
             // Assert
-            var statusCodeResult = Assert.IsType<NotFoundResult>(result);
-            Assert.Equal(404, statusCodeResult.StatusCode);
+            var statusCodeResult = Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, statusCodeResult.StatusCode);
         }
 
         [Fact]
         public async Task CreateProduct_ReturnsStatusCode201()
         {
             // Arrange
-            var controller = new Products(); // Assuming no constructor parameters needed
+            var controller = new Products();
 
             // Act
-            var result = await controller.CreateProduct(new ProductsDTO() { product_id = 5, product_name = "x", product_price = 4 }); // Pass appropriate DTO data
+            var result = await controller.CreateProduct(new ProductsDTO() { product_id = 253, product_name = "xx", product_price = 4 }); 
 
             // Assert
             var statusCodeResult = Assert.IsType<ObjectResult>(result);
@@ -53,14 +56,14 @@ namespace MFB_UnitTests
         public async Task DeleteProduct_ReturnsStatusCode204()
         {
             // Arrange
-            var controller = new Products(); // Assuming no constructor parameters needed
+            var controller = new Products();
 
             // Act
-            var result = await controller.DeleteProduct(6); // Pass an appropriate ID
+            var result = await controller.DeleteProduct(27);
 
             // Assert
-            var statusCodeResult = Assert.IsType<NotFoundResult>(result);
-            Assert.Equal(404, statusCodeResult.StatusCode);
+            var statusCodeResult = Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, statusCodeResult.StatusCode);
         }
 
 
